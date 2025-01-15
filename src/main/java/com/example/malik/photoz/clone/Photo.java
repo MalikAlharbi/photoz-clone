32adfa7aa2dc196795097740e0726e49081269ae
MalikAlharbi <malik.f.alharbi@gmail.com>
2025-01-15 20:34:26 +0300

    Upload & Download an image

--- src/main/java/com/example/malik/photoz/clone/Photo.java
+++ src/main/java/com/example/malik/photoz/clone/Photo.java
@@ -1,11 +1,25 @@
 package com.example.malik.photoz.clone;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.NotEmpty;
 
 public class Photo {
     private String id;
     @NotEmpty
     private String fileName;
+    @JsonIgnore
+    private byte[] data;
+
+    private String contentType;
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
 
     public Photo() {}
 
@@ -29,4 +43,13 @@ public class Photo {
     public void setFileName(String fileName) {
         this.fileName = fileName;
     }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
 }
--- src/main/java/com/example/malik/photoz/clone/PhotozController.java
+++ src/main/java/com/example/malik/photoz/clone/PhotozController.java
@@ -1,18 +1,24 @@
 package com.example.malik.photoz.clone;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.*;
 
 @RestController
 public class PhotozController {
 
-    private Map<String, Photo> db = new HashMap<>() {{
-        put("1", new Photo ( "1","hello.jpg"));
-    }};
+    private final PhotozService photozService;
+
+    @Autowired
+    public PhotozController(PhotozService photozService) {
+        this.photozService = photozService;
+    }
 
     @GetMapping("/")
     public String helloWorld() {
@@ -21,28 +27,26 @@ public class PhotozController {
 
     @GetMapping("/photoz")
     public Collection<Photo> get() {
-        return db.values();
+        return photozService.get();
     }
 
 
     @GetMapping("/photoz/{id}")
     public Photo get(@PathVariable String id) {
-        Photo photo = db.get(id);
+        Photo photo = photozService.get(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         return photo;
     }
 
     @DeleteMapping("/photoz/{id}")
     public void delete(@PathVariable String id) {
-        Photo photo = db.remove(id);
+        Photo photo = photozService.remove(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping("/photoz")
-    public Photo post(@RequestBody @Valid Photo photo) {
-        photo.setId(UUID.randomUUID().toString());
-        db.put(photo.getId(), photo);
-        return photo;
+    public Photo post(@RequestPart("data") MultipartFile file) throws IOException {
+        return photozService.save(file.getOriginalFilename(), file.getContentType(), file.getBytes());
     }
 
 }
